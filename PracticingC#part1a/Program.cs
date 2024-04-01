5dbc559df04e80a79b7dcd216a73fe98856fc4de
Stephen Smith <ssmith_138823@outlook.com>
2024-03-31 21:17:55 -0400

    pushing repo

---
+++ PracticingC#part1a/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I'm learning to code in C#!");
+            Console.WriteLine("its harder than I thought it would be!");
+            Console.WriteLine("but practicing will help!");
+        }
+    }
+}
